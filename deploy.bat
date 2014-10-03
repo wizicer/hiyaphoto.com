c9acd7714833c794aa62b72b3d8472146ef0cdab
Icer <liangshuangde@163.com>
2014-10-03 21:45:45 +0800

    add theme as a submodule change deploy.bat and try qiniu sync

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
 @echo off
 copy theme_config.yml themes\iceman\_config.yml /y
+copy introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
 call hexo generate
 call hexo o -d
 rd .deploy /s /q
