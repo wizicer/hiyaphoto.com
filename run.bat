40fe3b02872d1ed8f92f0fcfb570063329b432bb
Icer <liangshuangde@163.com>
2014-10-08 21:37:34 +0800

    update info, improve photo wall

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 del db.json
+copy hiya.jpg themes\iceman\source\img\hiya.jpg /y
 copy theme_config.yml themes\iceman\_config.yml /y
 copy introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
 call hexo server -d
