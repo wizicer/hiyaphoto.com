f50422c6bc1fb24aaf44770cf243f765f58583ca
Icer <liangshuangde@163.com>
2015-09-21 21:15:19 +0800

    scriptify theme resource

--- run.bat
+++ run.bat
@@ -1,6 +1,9 @@
 @echo off
 del db.json
-copy hiya.jpg themes\iceman\source\img\hiya.jpg /y
-copy theme_config.yml themes\iceman\_config.yml /y
-copy introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
+del themes\iceman\source\img\favicon.ico
+copy theme_resource\favicon.png themes\iceman\source\img\favicon.png /y
+copy theme_resource\hiya.jpg themes\iceman\source\img\hiya.jpg /y
+copy theme_resource\us.jpg themes\iceman\source\img\us.jpg /y
+copy theme_resource\config.yml themes\iceman\_config.yml /y
+copy theme_resource\introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
 call hexo server -d
