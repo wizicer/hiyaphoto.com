4d72b1e61a14cb2b9c6fb6f8daee90def8f9ce2d
Icer <liangshuangde@163.com>
2016-01-01 18:19:59 +0800

    sync with icersite

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,6 @@
 copy theme_config.yml themes\iceman\_config.yml /y
 copy introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
 call hexo generate
-rem call hexo o -d
-call hexo deploy
+call hexo o -d
 rd .deploy /s /q
 pause finished.
