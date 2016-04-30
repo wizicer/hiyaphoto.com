61e17f933f75e2746b97e192c37ce951706222c7
Icer <liangshuangde@163.com>
2016-04-30 22:07:44 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -5,5 +5,6 @@ copy theme_resource\hiya.jpg themes\iceman\source\img\hiya.jpg /y
 copy theme_resource\us.jpg themes\iceman\source\img\us.jpg /y
 copy theme_resource\config.yml themes\iceman\_config.yml /y
 copy theme_resource\introduction.ejs themes\iceman\layout\_widget\introduction.ejs /y
+call hexo generate
 call hexo deploy
 pause finished.
